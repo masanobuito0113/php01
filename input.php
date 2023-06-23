123ca016e82888cd78bfb522396638b890ddd8cf
masanobuito0113 <japancatclub@gmail.com>
2023-06-23 15:21:42 +0900

    php# Your branch is up to date with 'origin/main'.
    ppppp

--- input.php
+++ input.php
@@ -3,23 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>思い出記録</title>
 </head>
 <body>
-    <h1>デート記録フォーム</h1>
-    <ul>
+    <div class="container w-75">
+    <h2 class="text-center text-info mx-4 my-4">デート記録フォーム</h2>
+    <ul class="my-4">
     <form action="write.php" method="post">
-       <li><label for="start">デートした日時:</label>
+       <li class="my-4"><label for="start">デートした日時:</label>
         <input type="date" name="day"
          value="2023-06-22"
          min="2000-01-01" max="2040-12-31"></li>
-        <li>行った場所: <input type="text" name="place"></li>
-        <li>デートコース: <input type="text" name="course"></li>
-        <li>食べたもの:<input type="text" name="eat"></li>
-        <li>その他の思い出: <input type="text" name="other"></li>
+        <li class="my-4">メインで行った場所： <input class="w-75" type="text" name="place"></li>
+        <li class="my-4">デートコース： <input class="w-10" type="text" name="courseA">→<input class="w-10" type="text" name="courseB">→<input class="w-10" type="text" name="courseC"></li>
+        <li class="my-4">食べたもの：<input class="w-75" type="text" name="eat"></li>
+        <li class="my-4">その他の思い出： <input class="w-75" type="text" name="other"></li>
         </ul>
         <input type="submit" value="送信">
         </form>
-    
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
--- write.php
+++ write.php
@@ -2,11 +2,13 @@
 
 $day = $_POST['day'];
 $place = $_POST['place'];
-$course = $_POST['course'];
+$courseA = $_POST['courseA'];
+$courseB = $_POST['courseB'];
+$courseC = $_POST['courseC'];
 $eat = $_POST['eat'];
 $other = $_POST['other'];
 
-$data = $day . $place . $course . $eat . $other . "\n";
+$data = $day . $place . $courseA . $courseB . $courseC . $eat . $other . "\n";
 file_put_contents('data/data.txt', $data, FILE_APPEND)
 
 ?> 
